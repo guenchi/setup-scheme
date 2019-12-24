658fd1435b38359f5c474713d25b63a52fdc7439
guenchi <louis.deguenchy@gmail.com>
2019-12-24 02:51:32 +0100

    Update install.js

--- install.js
+++ install.js
@@ -38,7 +38,8 @@ async function main() {
                 break;
         }
         if(option === 'raven'){
-            await exec('curl -L http://ravensc.com/install | sudo scheme');
+            await exec('curl -L http://ravensc.com/install');
+            await exec('scheme install.sc'); 
         }
     }
   
