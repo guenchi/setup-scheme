e4e76b922e8233774261fdd05ebe8d0c217ac288
guenchi <louis.deguenchy@gmail.com>
2019-12-25 00:41:52 +0100

    Revert "Update install.js"
    This reverts commit cb4db9566b09ac4a0ef823fec359103fbbd06696.

--- install.js
+++ install.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const {exec} = require('@actions/exec');
+const path = require('path');
 
 
 main().catch(err =>{
@@ -13,14 +14,22 @@ async function main() {
     const version = core.getInput('version');
     const option = core.getInput('option');
     
+
+
     if (process.platform === 'darwin') {
         switch (implementation) {
 
             case'chez':
                 await exec('brew install chezscheme');
+                if(option === 'raven'){
+                await exec('ln -s /usr/local/bin/chez /usr/local/bin/scheme');
+                }
                 break;
         }
-
+        if(option === 'raven'){
+            await exec('curl -L http://ravensc.com/install');
+            await exec('scheme',[path.join(__dirname, 'install.sc')]);
+        }
     } else if (process.platform === 'linux') {
         switch (implementation) {
 
@@ -28,10 +37,12 @@ async function main() {
                 await exec('sudo apt install chezscheme');
                 break;
         }
-        
+        // if(option === 'raven'){
+        //     await exec('curl -L http://ravensc.com/install | sudo scheme');
+        // }
     }
   
-
+    
 
   
 }
